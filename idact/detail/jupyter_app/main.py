8e3dec1555262c57df6b443e3ec3df51183ce0ee
PiekarzKarolina <piekarz.karolina@gmail.com>
2019-06-20 20:21:07 +0200

    Return to the division into main and main_click for jupyter app

--- idact/detail/jupyter_app/main.py
+++ idact/detail/jupyter_app/main.py
@@ -34,59 +34,6 @@ from idact.detail.log.get_logger import get_logger
 SNIPPET_SEPARATOR_LENGTH = 10
 
 
-@click.command()
-@click.argument('cluster_name',
-                type=str)
-@click.option('--environment', '-e',
-              default=None,
-              type=str,
-              help="Environment path. Default: ~/.idact.conf"
-                   " or the value of IDACT_CONFIG_PATH.")
-@click.option('--save-defaults',
-              is_flag=True,
-              help="Save allocation parameters as defaults for next time.")
-@click.option('--reset-defaults',
-              is_flag=True,
-              help="Reset unspecified allocation parameters to defaults.")
-@click.option('--nodes',
-              default=None,
-              type=int,
-              help="Cluster node count. [Allocation parameter]."
-                   " Jupyter notebook will be deployed on the first node."
-                   " Default: 1.")
-@click.option('--cores',
-              default=None,
-              type=int,
-              help="CPU core count per node. [Allocation parameter]."
-                   " Default: 1")
-@click.option('--memory-per-node',
-              default=None,
-              type=str,
-              help="Memory per node. [Allocation parameter]."
-                   " Default: 1GiB")
-@click.option('--walltime',
-              default=None,
-              type=str,
-              help="Maximum time to allocate"
-                   " the resources for. Format: [days-]hours:minutes:seconds."
-                   " [Allocation parameter]."
-                   " Default: 0:10:00")
-@click.option('--native-arg',
-              nargs=2,
-              multiple=True,
-              type=str,
-              metavar='ARG VALUE',
-              default=[],
-              help="Native arguments for the workload manager."
-                   " Flags have value=None, e.g. --native-arg --flag None."
-                   " Can be repeated for multiple native args:"
-                   " --native-arg -arg1 v1 --native-arg -arg2 v2 (...)."
-                   " Values are not validated."
-                   " Supported arguments take precedence over native"
-                   " arguments."
-                   " Arguments specified later override earlier arguments."
-                   " [Allocation parameter]."
-                   " Default: No native arguments.")
 def main(cluster_name: str,
          environment: Optional[str],
          save_defaults: bool,
