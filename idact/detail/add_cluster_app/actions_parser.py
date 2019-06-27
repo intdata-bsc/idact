60dcce85ddf013031e9ebc35e3c70ac83b182853
PiekarzKarolina <piekarz.karolina@gmail.com>
2019-06-27 13:44:35 +0200

    Fix add_cluster tests and actions_parser logic

--- idact/detail/add_cluster_app/actions_parser.py
+++ idact/detail/add_cluster_app/actions_parser.py
@@ -4,5 +4,5 @@ def parse_actions(file_name: str):
     line after line.
     """
     with open(file_name) as file:
-        list_of_actions = file.readlines()
+        list_of_actions = file.read().split('\n')
     return list_of_actions
